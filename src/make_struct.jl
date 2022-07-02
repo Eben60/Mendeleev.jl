57f1f771d1ad71713daf5b4a638530a4bc2b4e8d
Eben60 <8xe8n-cm@online.de>
2022-07-02 21:51:26 +0200

    make_struct( ... ftypes::FT)

--- src/make_struct.jl
+++ src/make_struct.jl
@@ -1,19 +1,20 @@
 # https://github.com/PainterQubits/Unitful.jl/issues/385
 # quantity(FloatType, unit) = Quantity{FloatType, dimension(unit), typeof(unit)}
 
-function make_struct(sname, fnames, ftypes)
 
-    function make_where(fts, Ts)
-        w = ["$t <: $ft" for (t, ft) in zip(Ts, fts)]
-        w = join(w, ", ")
-        return "{$w}"
-    end
+function make_where(fts, Ts)
+    w = ["$t <: $ft" for (t, ft) in zip(Ts, fts)]
+    w = join(w, ", ")
+    return "{$w}"
+end
+
+function make_struct(sname, fnames, ftypes::FT) where FT <: Vector{T} where T<:Type
     sname = string(sname)
     length(fnames) != length(ftypes) && throw(ArgumentError("inequal length of fnames and ftypes vectors"))
-    ftypes = ftypes .|> Symbol .|> string
+    fts = ftypes .|> Symbol .|> string
     fnames = fnames .|> string
     Ts = ["T$i" for i in eachindex(fnames)]
-    w = make_where(ftypes, Ts)
+    w = make_where(fts, Ts)
 
     nmtp = Meta.parse("$sname$w")
     xs = ["$f::$t" for (f,t) in zip(fnames, Ts)]
@@ -27,5 +28,35 @@ function make_struct(sname, fnames, ftypes)
 
     return nothing
 end
+
+# # # WIP!
+function make_struct(sname, fnames, ftypes::FT) where FT <: Vector{T1} where T1 <: Vector{T} where T
+
+    sname = string(sname)
+    length(fnames) != length(ftypes) && throw(ArgumentError("inequal length of fnames and ftypes vectors"))
+
+    #ftypes = ftypes .|> Symbol .|> string
+
+    ftypes = [""]
+
+    fnames = fnames .|> string
+    Ts = ["T$i" for i in eachindex(fnames)]
+    w = make_where(fts, Ts)
+
+    nmtp = Meta.parse("$sname$w")
+    xs = ["$f::$t" for (f,t) in zip(fnames, Ts)]
+    x = Meta.parse.(xs)
+
+    @eval begin
+        struct $(nmtp)
+            $(x...)
+        end
+    end
+
+    return nothing
+end
+
+
+
 
 ms = make_struct
