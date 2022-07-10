b71a15b35f60890b5c982b8e4d4b3e3464518918
Eben60 <8xe8n-cm@online.de>
2022-07-10 19:36:14 +0200

    add Scratch

--- src/Mendeleev.jl
+++ src/Mendeleev.jl
@@ -1,5 +1,11 @@
 module Mendeleev
-using SQLite, DataFrames, PeriodicTable, Unitful
+using SQLite, DataFrames, PeriodicTable, Unitful, Scratch
+
+# https://discourse.julialang.org/t/julia-depot-path-and-julia-project/73745/26
+# https://discourse.julialang.org/t/using-scratch-space-as-a-time-limited-cache/76849
+# https://discourse.julialang.org/t/ann-scratch-jl-package-specific-mutable-data-containers/45855
+# https://github.com/JuliaPackaging/Scratch.jl
+# using Scratch
 
 include("constants.jl")
 include("make_struct.jl")
