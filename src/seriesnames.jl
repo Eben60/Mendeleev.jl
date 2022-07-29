e117dec914c6208989dd054dd2be22dad2f80ea8
Eben60 <8xe8n-cm@online.de>
2022-07-29 21:44:07 +0200

    replace vector by tuple

--- src/seriesnames.jl
+++ src/seriesnames.jl
@@ -1,3 +1,3 @@
-const seriesnames = ["Nonmetals", "Noble gases", "Alkali metals", "Alkaline earth metals",
+const seriesnames = ("Nonmetals", "Noble gases", "Alkali metals", "Alkaline earth metals",
                     "Metalloids", "Halogens", "Poor metals", "Transition metals",
-                    "Lanthanides", "Actinides"]
+                    "Lanthanides", "Actinides")
