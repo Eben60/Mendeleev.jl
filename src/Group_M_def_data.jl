7b83ee511ba60d3d4bd370948d4fb2abacf2841b
Eben60 <8xe8n-cm@online.de>
2022-07-30 19:16:07 +0200

    add group number (1 to 18)

--- src/Group_M_def_data.jl
+++ src/Group_M_def_data.jl
@@ -1,13 +1,15 @@
 struct Group_M
+    no::Int
     symbol::String
     name::String
 end
 
 function group_fullname(g::Group_M)
+    no = g.no
     name = g.name
     symbol = g.symbol
-    isempty(name) && return symbol
-    return "$symbol ($name)"
+    isempty(name) && return "$no - $symbol"
+    return "$no - $symbol ($name)"
 end
 
 
@@ -16,22 +18,22 @@ function Base.show(io::IO, g::Group_M)
 end
 
 groups_m = (
-Group_M("IA", "Alkali metals"),
-Group_M("IIA", "Alkaline earths"),
-Group_M("IIIB", ""),
-Group_M("IVB", ""),
-Group_M("VB", ""),
-Group_M("VIB", ""),
-Group_M("VIIB", ""),
-Group_M("VIIIB", ""),
-Group_M("VIIIB", ""),
-Group_M("VIIIB", ""),
-Group_M("IB", "Coinage metals"),
-Group_M("IIB", ""),
-Group_M("IIIA", "Boron group"),
-Group_M("IVA", "Carbon group"),
-Group_M("VA", "Pnictogens"),
-Group_M("VIA", "Chalcogens"),
-Group_M("VIIA", "Halogens"),
-Group_M("VIIIA", "Noble gases"),
+Group_M(1, "IA", "Alkali metals"),
+Group_M(2, "IIA", "Alkaline earths"),
+Group_M(3, "IIIB", ""),
+Group_M(4, "IVB", ""),
+Group_M(5, "VB", ""),
+Group_M(6, "VIB", ""),
+Group_M(7, "VIIB", ""),
+Group_M(8, "VIIIB", ""),
+Group_M(9, "VIIIB", ""),
+Group_M(10, "VIIIB", ""),
+Group_M(11, "IB", "Coinage metals"),
+Group_M(12, "IIB", ""),
+Group_M(13, "IIIA", "Boron group"),
+Group_M(14, "IVA", "Carbon group"),
+Group_M(15, "VA", "Pnictogens"),
+Group_M(16, "VIA", "Chalcogens"),
+Group_M(17, "VIIA", "Halogens"),
+Group_M(18, "VIIIA", "Noble gases"),
 )
