54e2ea29f8fce04860efa0538d1bcd9ddf1ac055
Eben60 <8xe8n-cm@online.de>
2022-07-31 22:54:19 +0200

    swap orb_type and orbital

--- src/screeniningconsts_def.jl
+++ src/screeniningconsts_def.jl
@@ -10,13 +10,13 @@ const orb_typenames = Dict([v=>k for (k, v) in zip(keys(orb_typenums), values(or
 
 struct ScreenConst
     atomic_number::Int
-    orb_type::Int
     shell::Int
+    orb_type::Int
     val::Float64
 end
 
-ScreenConst(a, ss::AbstractChar, s, v) = ScreenConst(a, orb_typenums[ss], s, v)
-ScreenConst(a, ss::T, s, v) where T <: Union{AbstractString, Symbol} = ScreenConst(a, orb_typenums[String(ss)[1]], s, v)
+ScreenConst(a, s, ss::AbstractChar, v) = ScreenConst(a, s, orb_typenums[ss], v)
+ScreenConst(a, s, ss::T, v) where T <: Union{AbstractString, Symbol} = ScreenConst(a, s, orb_typenums[String(ss)[1]], v)
 
 
 function Base.show(io::IO, sc::ScreenConst)
@@ -47,15 +47,15 @@ function Base.show(io::IO, scs::ScreenConstants)
 end
 
 # like getindex, but don't throw error
-function find_sc(s::ScreenConstants, orb_type, shell::Int)
-    sc0 = ScreenConst(s.atomic_number, orb_type, shell, NaN) # make dummy
+function find_sc(s::ScreenConstants, shell::Int, orb_type)
+    sc0 = ScreenConst(s.atomic_number, shell, orb_type, NaN) # make dummy
     pos = searchsorted(s.data, sc0)
     isempty(pos) && return (; sc = nothing, sc0)
     return (; sc = s.data[pos[begin]], sc0)
 end
 
-function Base.getindex(s::ScreenConstants, orb_type, shell::Int)
-    sc = find_sc(s, orb_type, shell)
+function Base.getindex(s::ScreenConstants, shell::Int, orb_type)
+    sc = find_sc(s, shell, orb_type)
     isnothing(sc.sc) && throw(KeyError("$(sc.sc0.shell)$(orb_typenames[sc.sc0.orb_type])"))
     return sc.sc
 end
@@ -64,10 +64,10 @@ end
 ################
 # tests
 
-Fes1 = ScreenConst(26, 's', 1, 4)
-Fes2 = ScreenConst(26, 's', 2, 4)
-Fep1 = ScreenConst(26, 'p', 1, 4)
-Ni = ScreenConst(28, 's', 1, 4)
+Fes1 = ScreenConst(26, 1, 's', 4)
+Fes2 = ScreenConst(26, 2, 's', 4)
+Fep1 = ScreenConst(26, 1, 'p', 4)
+Ni = ScreenConst(28, 1, 's', 4)
 
 scs = ScreenConstants([Fes1, Fes2, Fep1])
 
@@ -78,7 +78,7 @@ scs = ScreenConstants([Fes1, Fes2, Fep1])
 @test Fes1 < Fes2
 @test Fes1 < Fep1
 @test !(Fep1 < Fes1)
-@test SC.scs[1,1] == ScreenConst(26, 1,1, 4.0)
+@test SC.scs[2,1] == ScreenConst(26, 2,1, 4.0)
 
 # # # #
 end
