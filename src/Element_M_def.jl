7e3e98caa31fcf539610c738473ccb2e7e69e1fd
Eben60 <8xe8n-cm@online.de>
2022-08-27 19:23:41 +0200

    make Element_M non-parametric type

--- src/Element_M_def.jl
+++ src/Element_M_def.jl
@@ -1,86 +1,86 @@
 # this is computer generated file - better not edit
 
-struct Element_M{T1 <: Union{Missing, typeof(1.0*u"mg*kg^-1")}, T2 <: Union{Missing, typeof(1.0*u"mg*L^-1")}, T3 <: String, T4 <: Union{Missing, String}, T5 <: Int64, T6 <: Union{Missing, typeof(1.0*u"pm")}, T7 <: Union{Missing, typeof(1.0*u"pm")}, T8 <: Union{Missing, typeof(1.0*u"cm^3*mol^-1")}, T9 <: typeof(1.0*u"u"), T10 <: Union{Missing, Float64}, T11 <: String, T12 <: Union{Missing, typeof(1.0*u"K")}, T13 <: Union{Missing, Float64}, T14 <: Union{Missing, Float64}, T15 <: String, T16 <: Union{Missing, String}, T17 <: Union{Missing, typeof(1.0*u"pm")}, T18 <: Union{Missing, typeof(1.0*u"pm")}, T19 <: typeof(1.0*u"pm"), T20 <: Union{Missing, typeof(1.0*u"pm")}, T21 <: Union{Missing, typeof(1.0*u"pm")}, T22 <: Union{Missing, String}, T23 <: typeof(1.0*u"g*cm^-3"), T24 <: Union{Missing, String}, T25 <: Union{Missing, Float64}, T26 <: Union{Missing, Float64}, T27 <: String, T28 <: Union{Missing, String}, T29 <: Union{Missing, Int64}, T30 <: Union{Missing, String}, T31 <: Union{Missing, typeof(1.0*u"eV")}, T32 <: String, T33 <: Union{Missing, typeof(1.0*u"eV")}, T34 <: Union{Missing, Float64}, T35 <: Union{Missing, Float64}, T36 <: Union{Missing, typeof(1.0*u"kJ*mol^-1")}, T37 <: Union{Missing, typeof(1.0*u"kJ*mol^-1")}, T38 <: Union{Missing, typeof(1.0*u"kJ*mol^-1")}, T39 <: Union{Missing, String}, T40 <: Union{Missing, Int64}, T41 <: String, T42 <: Union{Missing, Int64}, T43 <: Union{Missing, typeof(1.0*u"kJ*mol^-1")}, T44 <: Bool, T45 <: Bool, T46 <: Union{Missing, String}, T47 <: Union{Missing, typeof(1.0*u"Å")}, T48 <: Union{Missing, String}, T49 <: Union{Missing, typeof(1.0*u"K")}, T50 <: Int64, T51 <: Union{Missing, typeof(1.0*u"pm")}, T52 <: Union{Missing, typeof(1.0*u"pm")}, T53 <: Union{Missing, typeof(1.0*u"J*mol*K^-1")}, T54 <: Union{Missing, String}, T55 <: String, T56 <: String, T57 <: Union{Missing, String}, T58 <: Int64, T59 <: Union{Missing, Int64}, T60 <: Union{Missing, String}, T61 <: Union{Missing, typeof(1.0*u"kJ*mol^-1")}, T62 <: Int64, T63 <: Union{Missing, Vector{Int64}}, T64 <: String, T65 <: Union{Missing, typeof(1.0*u"J*g^-1*K^-1")}, T66 <: Union{Missing, String}, T67 <: Union{Missing, String}, T68 <: Symbol, T69 <: Union{Missing, typeof(1.0*u"W*K^-1*m^-1")}, T70 <: Union{Missing, String}, T71 <: Union{Missing, typeof(1.0*u"pm")}, T72 <: Union{Missing, typeof(1.0*u"pm")}, T73 <: Union{Missing, typeof(1.0*u"pm")}, T74 <: Union{Missing, typeof(1.0*u"pm")}, T75 <: Union{Missing, typeof(1.0*u"pm")}, T76 <: Union{Missing, typeof(1.0*u"pm")}, T77 <: Union{Missing, typeof(1.0*u"pm")}, T78 <: Union{Missing, typeof(1.0*u"pm")}, T79 <: Union{Missing, typeof(1.0*u"pm")}, T80 <: Union{Missing, String}, T81 <: Union{Missing, Int64}, T82 <: Union{Missing, Int64}}
-    abundance_crust::T1
-    abundance_sea::T2
-    annotation::T3
-    appearance::T4
-    atomic_number::T5
-    atomic_radius::T6
-    atomic_radius_rahm::T7
-    atomic_volume::T8
-    atomic_weight::T9
-    atomic_weight_uncertainty::T10
-    block::T11
-    boiling_point::T12
-    c6::T13
-    c6_gb::T14
-    cas::T15
-    category::T16
-    covalent_radius_bragg::T17
-    covalent_radius_cordero::T18
-    covalent_radius_pyykko::T19
-    covalent_radius_pyykko_double::T20
-    covalent_radius_pyykko_triple::T21
-    cpk_color::T22
-    density::T23
-    description::T24
-    dipole_polarizability::T25
-    dipole_polarizability_unc::T26
-    discoverers::T27
-    discovery_location::T28
-    discovery_year::T29
-    el_config::T30
-    electron_affinity::T31
-    electronic_configuration::T32
-    en_allen::T33
-    en_ghosh::T34
-    en_pauling::T35
-    evaporation_heat::T36
-    fusion_heat::T37
-    gas_basicity::T38
-    geochemical_class::T39
-    glawe_number::T40
-    goldschmidt_class::T41
-    group::T42
-    heat_of_formation::T43
-    is_monoisotopic::T44
-    is_radioactive::T45
-    jmol_color::T46
-    lattice_constant::T47
-    lattice_structure::T48
-    melting_point::T49
-    mendeleev_number::T50
-    metallic_radius::T51
-    metallic_radius_c12::T52
-    molar_heat_capacity::T53
-    molcas_gv_color::T54
-    name::T55
-    name_origin::T56
-    nist_webbook::T57
-    period::T58
-    pettifor_number::T59
-    phase::T60
-    proton_affinity::T61
-    series::T62
-    shells::T63
-    sources::T64
-    specific_heat_capacity::T65
-    spectral_img::T66
-    summary::T67
-    symbol::T68
-    thermal_conductivity::T69
-    uses::T70
-    vdw_radius::T71
-    vdw_radius_alvarez::T72
-    vdw_radius_batsanov::T73
-    vdw_radius_bondi::T74
-    vdw_radius_dreiding::T75
-    vdw_radius_mm3::T76
-    vdw_radius_rt::T77
-    vdw_radius_truhlar::T78
-    vdw_radius_uff::T79
-    wikipedia::T80
-    xpos::T81
-    ypos::T82
+struct Element_M
+    abundance_crust::Union{Missing, typeof(1.0*u"mg*kg^-1")}
+    abundance_sea::Union{Missing, typeof(1.0*u"mg*L^-1")}
+    annotation::String
+    appearance::Union{Missing, String}
+    atomic_number::Int64
+    atomic_radius::Union{Missing, typeof(1.0*u"pm")}
+    atomic_radius_rahm::Union{Missing, typeof(1.0*u"pm")}
+    atomic_volume::Union{Missing, typeof(1.0*u"cm^3*mol^-1")}
+    atomic_weight::typeof(1.0*u"u")
+    atomic_weight_uncertainty::Union{Missing, Float64}
+    block::String
+    boiling_point::Union{Missing, typeof(1.0*u"K")}
+    c6::Union{Missing, Float64}
+    c6_gb::Union{Missing, Float64}
+    cas::String
+    category::Union{Missing, String}
+    covalent_radius_bragg::Union{Missing, typeof(1.0*u"pm")}
+    covalent_radius_cordero::Union{Missing, typeof(1.0*u"pm")}
+    covalent_radius_pyykko::typeof(1.0*u"pm")
+    covalent_radius_pyykko_double::Union{Missing, typeof(1.0*u"pm")}
+    covalent_radius_pyykko_triple::Union{Missing, typeof(1.0*u"pm")}
+    cpk_color::Union{Missing, String}
+    density::typeof(1.0*u"g*cm^-3")
+    description::Union{Missing, String}
+    dipole_polarizability::Union{Missing, Float64}
+    dipole_polarizability_unc::Union{Missing, Float64}
+    discoverers::String
+    discovery_location::Union{Missing, String}
+    discovery_year::Union{Missing, Int64}
+    el_config::Union{Missing, String}
+    electron_affinity::Union{Missing, typeof(1.0*u"eV")}
+    electronic_configuration::String
+    en_allen::Union{Missing, typeof(1.0*u"eV")}
+    en_ghosh::Union{Missing, Float64}
+    en_pauling::Union{Missing, Float64}
+    evaporation_heat::Union{Missing, typeof(1.0*u"kJ*mol^-1")}
+    fusion_heat::Union{Missing, typeof(1.0*u"kJ*mol^-1")}
+    gas_basicity::Union{Missing, typeof(1.0*u"kJ*mol^-1")}
+    geochemical_class::Union{Missing, String}
+    glawe_number::Union{Missing, Int64}
+    goldschmidt_class::String
+    group::Union{Missing, Int64}
+    heat_of_formation::Union{Missing, typeof(1.0*u"kJ*mol^-1")}
+    is_monoisotopic::Bool
+    is_radioactive::Bool
+    jmol_color::Union{Missing, String}
+    lattice_constant::Union{Missing, typeof(1.0*u"Å")}
+    lattice_structure::Union{Missing, String}
+    melting_point::Union{Missing, typeof(1.0*u"K")}
+    mendeleev_number::Int64
+    metallic_radius::Union{Missing, typeof(1.0*u"pm")}
+    metallic_radius_c12::Union{Missing, typeof(1.0*u"pm")}
+    molar_heat_capacity::Union{Missing, typeof(1.0*u"J*mol*K^-1")}
+    molcas_gv_color::Union{Missing, String}
+    name::String
+    name_origin::String
+    nist_webbook::Union{Missing, String}
+    period::Int64
+    pettifor_number::Union{Missing, Int64}
+    phase::Union{Missing, String}
+    proton_affinity::Union{Missing, typeof(1.0*u"kJ*mol^-1")}
+    series::Int64
+    shells::Union{Missing, Vector{Int64}}
+    sources::String
+    specific_heat_capacity::Union{Missing, typeof(1.0*u"J*g^-1*K^-1")}
+    spectral_img::Union{Missing, String}
+    summary::Union{Missing, String}
+    symbol::Symbol
+    thermal_conductivity::Union{Missing, typeof(1.0*u"W*K^-1*m^-1")}
+    uses::Union{Missing, String}
+    vdw_radius::Union{Missing, typeof(1.0*u"pm")}
+    vdw_radius_alvarez::Union{Missing, typeof(1.0*u"pm")}
+    vdw_radius_batsanov::Union{Missing, typeof(1.0*u"pm")}
+    vdw_radius_bondi::Union{Missing, typeof(1.0*u"pm")}
+    vdw_radius_dreiding::Union{Missing, typeof(1.0*u"pm")}
+    vdw_radius_mm3::Union{Missing, typeof(1.0*u"pm")}
+    vdw_radius_rt::Union{Missing, typeof(1.0*u"pm")}
+    vdw_radius_truhlar::Union{Missing, typeof(1.0*u"pm")}
+    vdw_radius_uff::Union{Missing, typeof(1.0*u"pm")}
+    wikipedia::Union{Missing, String}
+    xpos::Union{Missing, Int64}
+    ypos::Union{Missing, Int64}
 end
