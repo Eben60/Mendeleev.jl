e58c7053dbf65c9a672267ed7072829f7de089b0
Eben60 <8xe8n-cm@online.de>
2022-09-04 16:16:45 +0200

    WIP

--- src/Isotopes.jl
+++ src/Isotopes.jl
@@ -2,6 +2,8 @@ module IS
 using Mendeleev, Unitful, Tables, DataFrames, CSV
 using Main.Generate_MFiles
 
+using Main.Generate_MFiles: dfs, round_pos, LAST_NO
+
 isot_fl = "auxillary/isotopes.txt"
 
 els = ELEMENTS_M
@@ -23,7 +25,7 @@ end
 
 Isotope(x::Vector) = Isotope(x...)
 
-isot = Generate_MFiles.dfs.isotopes
+isot = dfs.isotopes
 i2 = select(isot, :id, :atomic_number, :mass_number, :mass, :abundance, :half_life)
 i3 = select(isot, :id, :atomic_number, :atomic_number => (x -> sym.(x))  => :sym, :mass_number, :mass, :abundance, :half_life)
 
@@ -42,12 +44,41 @@ hli = hlinf.(hl, ir)
 
 i4.half_life = hli
 
-i5 = i4[.!ismissing.(i4.abundance), [:atomic_number, :mass_number, :mass, :abundance, :is_radioactive, :half_life]]
+; i5 = i4[.!ismissing.(i4.abundance), [:atomic_number, :mass_number, :mass, :abundance, :is_radioactive, :half_life]]
+
+
+i4s = subset(i4, :abundance => (x -> .!ismissing.(x) ))[!, [:atomic_number, :mass_number, :mass, :abundance, :is_radioactive, :half_life]]
+
+
 
 i5t = i5 |> rowtable .|> Tuple .|> collect .|> Generate_MFiles.round_pos
+
+function elem_isotopes(no, df)
+    nos = Set(df[!, :atomic_number])
+    !(no in nos) && return missing
+    el_data = subset(df, :atomic_number => (x -> x .== no )) |> rowtable .|> Tuple .|> collect .|> round_pos
+    return el_data
+end
+
+i110 = elem_isotopes(110, i4s)
+i2 = elem_isotopes(2, i4s)
+
+d_isot = Dict(n => elem_isotopes(n, i4s) for n in 1:LAST_NO)
+
+function isot_string(x)
+    s = string.(x)
+    j = join(s, ", ")
+    return "Isotope($j)"
+end
+
+function isots_string(x)
+    j = join(isot_string.(x), ", ")
+    return "[$j]"
+end
+
 # i5tr = Generate_MFiles.round_pos.(i5t, 13)
 
-is = Isotope.(i5t)
+# is = Isotope.(i5t)
 
 # CSV.write(isot_fl, i4; delim='\t', missingstring="missing")
 
