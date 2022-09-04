4882b22f995963a00c96b26c228e25ef3531e1de
Eben60 <8xe8n-cm@online.de>
2022-09-04 17:59:19 +0200

    isotopes unitful

--- src/isotopes_def.jl
+++ src/isotopes_def.jl
@@ -1,10 +1,13 @@
 struct Isotope
     atomic_number::Int
     mass_number::Int
-    mass::Float64
+    mass::typeof(1.0*u"u")
     abundance::Union{Float64, Missing}
     is_radioactive::Bool
-    half_life::Union{Float64, Missing}
+    half_life::Union{typeof(1.0*u"yr"), Missing}
 end
+
+Isotope(atomic_number, mass_number, mass::Float64, abundance, is_radioactive, half_life::Union{Float64, Missing}) =
+    Isotope(atomic_number, mass_number, mass*u"u", abundance, is_radioactive, half_life*u"yr")
 
 # Isotope(x::Vector) = Isotope(x...)
