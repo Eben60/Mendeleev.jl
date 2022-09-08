b79c61c3b89ba95cd7f737424d51b5866acda0c7
Eben60 <8xe8n-cm@online.de>
2022-09-08 23:46:32 +0200

    overload for Isotopes struct

--- src/isotopes_def.jl
+++ src/isotopes_def.jl
@@ -11,3 +11,29 @@ Isotope(atomic_number, mass_number, mass::Float64, abundance) =
 struct Isotopes
     isotopes::Vector{Isotope}
 end
+
+Base.getindex(iss::Isotopes, i::Integer) = iss.isotopes[i]
+Base.getindex(iss::Isotopes, v::AbstractVector) = [iss[i] for i in v]
+
+Base.get(iss::Isotopes, i::Integer, default) = get(iss.isotopes[i], i, default)
+
+
+# support iterating over Isotopes
+Base.eltype(iss::Isotopes) = Isotope
+Base.length(iss::Isotopes) = length(iss.isotopes)
+Base.iterate(iss::Isotopes, state...) = iterate(iss.isotopes, state...)
+
+
+# Isotope equality is determined by atomic number and isotope mass number
+Base.isequal(is1::Isotope, is2::Isotope) = (i1.atomic_number, i1.mass_number) == (i1.atomic_number, i1.mass_number)
+
+# There is no need to use all the data in Isotope to calculated the hash
+# since Isotope equality is determined by atomic number and isotope mass number.
+Base.hash(is::Isotope, h::UInt) = hash((is.atomic_number, is.mass_number), h)
+
+# Compare isotopes by by atomic number and isotope mass number to produce the most common way isotopes
+# are sorted.
+Base.isless(is1::Isotope, is2::Isotope) = (i1.atomic_number, i1.mass_number) < (i1.atomic_number, i1.mass_number)
+
+# Provide a simple way to iterate over all isotopes.
+Base.eachindex(iss::Isotopes) = eachindex(iss.isotopes)
