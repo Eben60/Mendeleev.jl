5e1bb445408ddb9d0e6dc3c89431343f394061cf
Eben60 <8xe8n-cm@online.de>
2022-09-10 13:34:02 +0200

    printing isotopes

--- src/isotopes_def.jl
+++ src/isotopes_def.jl
@@ -44,3 +44,60 @@ function Base.show(io::IO, iss::Isotopes)
     println(io, "(", join(is_strings, ", "), ")")
     return nothing
 end
+
+function subsuperchar(c, sub)
+    subscripts = Dict(
+        '0' => '₀',
+        '1' => '₁',
+        '2' => '₂',
+        '3' => '₃',
+        '4' => '₄',
+        '5' => '₅',
+        '6' => '₆',
+        '7' => '₇',
+        '8' => '₈',
+        '9' => '₉',
+    )
+
+    superscripts = Dict(
+        '0' => '⁰',
+        '1' => '¹',
+        '2' => '²',
+        '3' => '³',
+        '4' => '⁴',
+        '5' => '⁵',
+        '6' => '⁶',
+        '7' => '⁷',
+        '8' => '⁸',
+        '9' => '⁹',
+    )
+    if sub
+        return subscripts[c]
+    else
+        return superscripts[c]
+    end
+end
+
+function subsuperstring(x, sub=false)
+    s = string(x)
+    carr = [subsuperchar(c, sub) for c in s]
+    return join(carr, "")
+end
+
+function Base.show(io::IO, is::Isotope)
+    # println("----")
+    # @show i = is.atomic_number
+    # @show el = ELEMENTS_M[i]
+    # @show sym = el.symbol
+    #
+    # @show i = is.atomic_number
+    # @show el = ELEMENTS_M[is.atomic_number].symbol
+    # @show sym = el.symbol
+    #
+
+    sym = ELEMENTS_M[is.atomic_number].symbol |> string
+    mass_no_superscript = subsuperstring(is.mass_number)
+
+    print(io, "($((is.abundance)*100)% $mass_no_superscript$sym m=$(is.mass) )")
+    return nothing
+end
