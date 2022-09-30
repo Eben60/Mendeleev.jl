ba76975b7c62e68cd6d06d7c1f2539977fba3da1
Eben60 <8xe8n-cm@online.de>
2022-09-30 20:31:15 +0200

    add path_in_Mend function

--- src/GenerateMendeleev/Generate_MFiles.jl
+++ src/GenerateMendeleev/Generate_MFiles.jl
@@ -6,11 +6,11 @@ using Scratch
 dev = false
 
 include("paths.jl")
-include("../units.jl") # part of Mendeleev
-include("../seriesnames.jl") # part of Mendeleev
-include("../Group_M_def_data.jl") # part of Mendeleev
-include("../synonym_fields.jl") # part of Mendeleev
-include("../screeniningconsts_def.jl") # part of Mendeleev
+include(path_in_Mend("units.jl")) # part of Mendeleev
+include(path_in_Mend("seriesnames.jl")) # part of Mendeleev
+include(path_in_Mend("Group_M_def_data.jl")) # part of Mendeleev
+include(path_in_Mend("synonym_fields.jl")) # part of Mendeleev
+include(path_in_Mend("screeniningconsts_def.jl")) # part of Mendeleev
 include("PeriodicTable2df.jl")
 include("make_struct.jl")
 include("utype2str.jl")
@@ -18,7 +18,7 @@ include("f_units.jl")
 include("data_import.jl")
 if ! dev
     write_struct_jl(struct_fl, s_def_text)
-    include("../Element_M_def.jl") # file just computer-generated - will be part of Mendeleev
+    include(path_in_Mend("Element_M_def.jl")) # file just computer-generated - will be part of Mendeleev
     include("Isotopes.jl")
 end
     include("make_static_data.jl")
--- src/GenerateMendeleev/paths.jl
+++ src/GenerateMendeleev/paths.jl
@@ -1,15 +1,23 @@
+# local files
 datadir = joinpath(@__DIR__ , "../../data/")
-
 elements_src = joinpath(datadir , "elements.db")
 tmp_dir = @get_scratch!("mendeleev_files")
 elements_dbfile = joinpath(tmp_dir, "mendeleev-elements.db")
 chembook_jsonfile = joinpath(datadir , "el_chembook.json")
 
-struct_fl = "src/Element_M_def.jl"
-static_data_fl = "src/elements_data.jl"
-oxstate_fl = "src/oxistates_data.jl"
-screening_fl = "src/screening_data.jl"
-ionization_fl = "src/ionization_data.jl"
-isotopes_fl = "src/isotopes_data.jl"
+# Mendeleev.jl files
+function path_in_Mend(fl, path=nothing)
+    if isnothing(path)
+        path = normpath(@__DIR__ , "../../src/")
+    end
+    return normpath(path, fl)
+end
+
+struct_fl = path_in_Mend("Element_M_def.jl")
+static_data_fl = path_in_Mend("elements_data.jl")
+oxstate_fl = path_in_Mend("oxistates_data.jl")
+screening_fl = path_in_Mend("screening_data.jl")
+ionization_fl = path_in_Mend("ionization_data.jl")
+isotopes_fl = path_in_Mend("isotopes_data.jl")
 
 # const intNaN = -9223372033146270158 # big negative random value as proxy for NaN / missing
