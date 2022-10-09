46eda007e8f1f07c062ff4f3d533abe3226b2415
Ben Elkin <8xe8n-cm@online.de>
2022-10-09 14:57:21 +0200

    manual coverage run

--- test/coverage/manual-coverage.jl
+++ test/coverage/manual-coverage.jl
@@ -4,12 +4,16 @@ Pkg.test("Mendeleev"; coverage=true)
 srcfolder = normpath(@__DIR__, "../../src")
 coverage = process_folder(srcfolder)
 
-coverage = merge_coverage_counts(coverage, filter!(
-    let prefixes = (joinpath(pwd(), "src", ""),
-                    joinpath(pwd(), "deps", ""))
-        c -> any(p -> startswith(c.filename, p), prefixes)
-    end,
-    LCOV.readfolder("test")))
-#
+open("lcov.info", "w") do io
+    LCOV.write(io, coverage)
+end;
 
-@show get_summary(process_file(joinpath("src", "Mendeleev.jl")))
+# coverage = merge_coverage_counts(coverage, filter!(
+#     let prefixes = (joinpath(pwd(), "src", ""),
+#                     joinpath(pwd(), "deps", ""))
+#         c -> any(p -> startswith(c.filename, p), prefixes)
+#     end,
+#     LCOV.readfolder("test")))
+# #
+
+# @show get_summary(process_file(joinpath("src", "Mendeleev.jl")))
