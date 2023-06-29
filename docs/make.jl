377b108b731ddb08cd868494fa87fbd5edcf5b24
Ben Elkin <8xe8n-cm@online.de>
2023-06-29 12:47:57 +0200

    update docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,8 @@
 # push!(LOAD_PATH,"../src/")
+using Pkg
+
+Pkg.activate("docs")
+
 using Documenter, Mendeleev
 
 # generate documentation locally. 
@@ -8,13 +12,15 @@ makedocs(
     format = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true"),
     authors = "Eben60",
     sitename = "Mendeleev.jl",
-    pages = Any["Home" => "index.md", "Elements Data Fields" => "elements_data_fields.md", "Types" => "types.md"]
+    pages = Any["Home" => "index.md", "Elements Data Fields" => "elements_data_fields.md", "Types" => "types.md", "Changelog" => "changelog.md"]
     # strict = true,
     # clean = true,
     # checkdocs = :exports,
 )
+;
 
 # depoloyment done on the server anyway
+# don't normally run deploydocs here
 deploydocs(
     repo = "github.com/Eben60/Mendeleev.jl.git",
     versions = nothing,
